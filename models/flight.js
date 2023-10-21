a6d791ea929809fd322a4f605a33e9959b2a2620
kfac <kfacison1998@gmail.com>
2023-10-20 19:26:42 -0500

    stoping point

--- models/flight.js
+++ models/flight.js
@@ -3,10 +3,25 @@ const mongoose =require('mongoose');
 const Schema = mongoose.Schema;
 
 const flightschema = new Schema({
-    airline: String,
-    airport: String,
-    flightNo: Number,
-    departs: Date,
+    airline: {
+        type: String,
+        enum: ['American', 'Southwest','United','Frontier']
+        },
+    airport: {
+        type: String,
+        default: 'DFW',
+        enum: ['AUS', 'DFW','DEN','LAX','SAN']
+        },
+    flightNo: {
+        type: Number,
+        required: true,
+        min: 10,
+        max: 9999
+    },
+    departs: {
+        type: Date,
+        default: '2024-10-31T11:00'
+    },
 }, {
     timestamps: true
 });
